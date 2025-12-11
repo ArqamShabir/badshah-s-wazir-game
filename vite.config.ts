95610cccb4b4304233bf943a2e84b09b8054f3a8
ArqamShabir <mrking6a@gmail.com>
2025-12-11 19:48:32 +0500

    logo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -13,6 +12,22 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: false,          // removes heavy map files
+    minify: "terser",          // better compression
+    target: "es2017",          // smaller output
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";   // makes vendor.js separate
+          }
+        },
+      },
     },
   },
 }));
